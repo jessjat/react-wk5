2119a2307e53f4aa9adc419e3f644019bae5be3d
jessjat <mutanujat@gmail.com>
2024-11-05 14:06:09 +0300

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+    <footer>
+      This project was coded by Jacinta Musau and 
+    <a href="https://github.com/jessjat/react-wk5"target="_blank">  is open-sourced on GitHub</a>
+    </footer>
+    </div>
   );
 }
 
