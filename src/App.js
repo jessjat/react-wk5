a66e52cb650fc2a3af4beb7b2f89f83683b35775
jessjat <mutanujat@gmail.com>
2024-11-05 14:09:39 +0300

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
       <h1>Weather App</h1>
     <footer>
       This project was coded by Jacinta Musau and 
-    <a href="https://github.com/jessjat/react-wk5"target="_blank">  is open-sourced on GitHub</a>
+    <a href="https://github.com/jessjat/react-wk5"target="_blank" rel="nonreferrer">  is open-sourced on GitHub</a>
     </footer>
     </div>
   );
