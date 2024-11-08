fbf8e1b378cb73f8431c7a97a704684bedfb14ca
jessjat <mutanujat@gmail.com>
2024-11-08 12:17:24 +0300

    Added a weatherInfo component

--- src/Weather.js
+++ src/Weather.js
@@ -20,10 +20,21 @@ export default function Weather(props){
             });
    
    }
+   function handleSubmit(event){
+      event.preventDefault();
+      search();
+   }
    function handleCityChange(event){
       setCity(event.target.value);
       
-   }
+      }
+
+   function search(){
+      const apiKey= "1266ad07b66517497b1acf79ea5a6a64";
+      const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse)
+
+   }   
 
    if(weatherData.ready){
    return(
@@ -38,39 +49,10 @@ export default function Weather(props){
             </div>
         </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-         <li>
-         <FormattedDate date={weatherData.date} />
-         </li> 
-         <li className="text-capitalize">{weatherData.description}</li>  
-        </ul>
-        <div className="row mt-3">
-         <div className="col-6">
-            <div className="clearfix">
-            <img src={weatherData.iconUrl}
-            alt={weatherData.description}
-            className="float-left" />
-            <div className="float-left">
-            <span className="temperature">{Math.round(weatherData.temperature)}</span>
-            <span className="unit">°C</span>
-            </div>
-            </div>
-            </div>
-            <div className="col-6">
-               <ul>
-                <li>{weatherData.humidity}%</li>
-                <li>{weatherData.wind}km/h</li>
-                </ul> 
-                </div>   
-        </div>
-        </div>
+       </div>
    ); 
 }else{
-   const apiKey= "1266ad07b66517497b1acf79ea5a6a64";
-   const apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-   axios.get(apiUrl).then(handleResponse);
-   
+  search();
    return "Loading...";
   }
 }
