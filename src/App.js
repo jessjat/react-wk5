83ea43e244410af482f0d29d6d8715333311a457
jessjat <mutanujat@gmail.com>
2024-11-08 17:17:11 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      
       <Weather defaultCity="Nairobi" />
 
     <footer>
       This project was coded by Jacinta Musau and 
-    <a href="https://github.com/jessjat/react-wk5"target="_blank" rel="noreferrer">  is open-sourced on GitHub</a>
+    <a href="https://github.com/jessjat/react-wk5"target="_blank" rel="noopener noreferrer">  is open-sourced on GitHub</a>
     </footer>
      </div>
     </div>
