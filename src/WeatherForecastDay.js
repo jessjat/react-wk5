4b178a35401a66dfe8d50ce7ad7876f097652956
jessjat <mutanujat@gmail.com>
2024-12-26 21:31:07 +0300

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -11,7 +11,7 @@ export default function WeatherForecastDay(props){
         return`${temperature}°`;
     } 
     function day(){
-        let date =new Date(props.data.temperature.day*1000);
+        let date =new Date(props.data.time * 1000);
         let day=date.getDay();
         let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
         return days[day];
